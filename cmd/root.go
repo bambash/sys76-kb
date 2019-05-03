7260796b7db76efcee01724245909b72fed0705e
bambash <bambash256@gmail.com>
2019-05-02 23:50:19 -0500

    adding cobra, making keyboard package

---
+++ cmd/root.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/bambash/sys76-kb/keyboard"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "sys76kb",
+	Short: "sys76-kb is a keyboard controller for System76 laptops",
+	Long: `A simple keyboard contoller built with
+				  love by bambash in Go.
+				  Complete documentation is available at https://github.com/bambash/sys76-kb`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := cmd.ParseFlags(args); err != nil {
+			fmt.Printf("Error parsing flags: %v\n", err)
+		}
+
+		keyboard.InfiniteRainbow()
+	},
+}
+
+// Execute is the primary entrypoint for this CLI
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ keyboard/keyboard.go
@@ -0,0 +1,121 @@
+package keyboard
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+type RGBColor struct {
+	Red   int
+	Green int
+	Blue  int
+}
+
+// GetHex Converts a decimal number to hex representations
+func getHex(num int) string {
+	hex := fmt.Sprintf("%X", num)
+	if len(hex) == 1 {
+		hex = "0" + hex
+	}
+	return hex
+}
+
+// GetColorInHex returns a color in HEX format
+func GetColorInHex(c RGBColor) string {
+	hex := getHex(c.Red) + getHex(c.Green) + getHex(c.Blue)
+	return hex
+}
+
+// ColorFileHandler writes a hex value to color_left, and returns the bytes written
+func ColorFileHandler(c string) int {
+	f, err := os.OpenFile("/sys/class/leds/system76::kbd_backlight/color_left", os.O_RDWR, 0755)
+	defer f.Close()
+
+	if err != nil {
+		log.Fatal(err)
+		return 0
+	}
+
+	l, err := f.WriteString(c)
+	if err != nil {
+		log.Fatal(err)
+		f.Close()
+		return 0
+	}
+
+	err = f.Close()
+	if err != nil {
+		log.Fatal(err)
+		return 0
+	}
+	return l
+}
+
+// BrightnessFileHandler writes a hex value to brightness, and returns the bytes written
+func BrightnessFileHandler(c string) int {
+	f, err := os.OpenFile("/sys/class/leds/system76::kbd_backlight/brightness", os.O_RDWR, 0755)
+	defer f.Close()
+
+	if err != nil {
+		log.Fatal(err)
+		return 0
+	}
+
+	l, err := f.WriteString(c)
+	if err != nil {
+		log.Fatal(err)
+		f.Close()
+		return 0
+	}
+
+	err = f.Close()
+	if err != nil {
+		log.Fatal(err)
+		return 0
+	}
+	return l
+}
+
+// InfiniteRainbow generates... an infinite rainbow
+func InfiniteRainbow() {
+
+	// infinite rainbow
+	for {
+		for i := 0; i <= 255; i++ {
+			c := RGBColor{255, i, 0}
+			hex := GetColorInHex(c)
+			ColorFileHandler(hex)
+		}
+
+		for i := 255; i >= 0; i-- {
+			c := RGBColor{i, 255, 0}
+			hex := GetColorInHex(c)
+			ColorFileHandler(hex)
+		}
+
+		for i := 0; i <= 255; i++ {
+			c := RGBColor{0, 255, i}
+			hex := GetColorInHex(c)
+			ColorFileHandler(hex)
+		}
+
+		for i := 255; i >= 0; i-- {
+			c := RGBColor{0, i, 255}
+			hex := GetColorInHex(c)
+			ColorFileHandler(hex)
+		}
+
+		for i := 0; i <= 255; i++ {
+			c := RGBColor{i, 0, 255}
+			hex := GetColorInHex(c)
+			ColorFileHandler(hex)
+		}
+
+		for i := 255; i >= 0; i-- {
+			c := RGBColor{255, 0, i}
+			hex := GetColorInHex(c)
+			ColorFileHandler(hex)
+		}
+	}
+}
